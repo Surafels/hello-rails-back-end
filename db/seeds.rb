d4c63a41d71fb2b3e85a1dc710405f3deec08277
Ssurafels <ssurafel4@gmail.com>
2024-02-08 15:29:21 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Message.create([
+    {message: 'Hello'},
+    {message:'Welcome!'},
+    {message: 'Greetings'},
+    {message: 'Hola!'},
+    {message: 'Salutations!'}
+
+])
